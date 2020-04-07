11dc0b43c8e4c3fa0853712d5caef7f1d8131eee
davidthaper <49223258+davidthaper@users.noreply.github.com>
2020-04-07 15:13:17 +0530

    External Storage

--- MainActivity.java
+++ MainActivity.java
@@ -1,64 +1,104 @@
-package com.david.InternalStorage1;
+package com.david.ExternalStorage1;
 
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Environment;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
+    EditText inputText;
+    TextView response;
+    Button saveButton,readButton;
 
-    EditText textmsg;
-    private String file= "my data";
-    static final int READ_BLOCK_SIZE = 100;
-    TextView tv;
+    private String filename = "SampleFile.txt";
+    private String filepath = "MyFileStorage";
+    File myExternalFile;
+    String myData = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv=findViewById(R.id.tv_read);
-        textmsg=(EditText)findViewById(R.id.editText1);
-    }
 
-    // write text to file
-    public void WriteBtn(View v) {
-        // add-write text into file
-        try {
-            FileOutputStream fileout=openFileOutput(file, MODE_PRIVATE);
-            OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
-            outputWriter.write(textmsg.getText().toString());
-            outputWriter.close();
+        inputText = (EditText) findViewById(R.id.myInputText);
+        response = (TextView) findViewById(R.id.response);
 
-            //display file saved message
-            Toast.makeText(getBaseContext(), "File saved successfully!",
-                    Toast.LENGTH_SHORT).show();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Read text from file
-    public void ReadBtn(View v) {
-        //reading text from file
-        try {
-            FileInputStream fin = openFileInput(file);
-            int c;
-            String temp="";
-            while( (c = fin.read()) != -1){
-                temp = temp + Character.toString((char)c);
+         saveButton =
+                (Button) findViewById(R.id.saveExternalStorage);
+        saveButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream fos = new FileOutputStream(myExternalFile);
+                    fos.write(inputText.getText().toString().getBytes());
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                inputText.setText("");
+                response.setText("SampleFile.txt saved to External Storage...");
             }
-            tv.setText(temp);
-            Toast.makeText(getBaseContext(),"file read",Toast.LENGTH_SHORT).show();
+        });
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        readButton = (Button) findViewById(R.id.getExternalStorage);
+        readButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileInputStream fis = new FileInputStream(myExternalFile);
+                    DataInputStream in = new DataInputStream(fis);
+                    BufferedReader br =
+                            new BufferedReader(new InputStreamReader(in));
+                    String strLine;
+                    while ((strLine = br.readLine()) != null) {
+                        myData = myData + strLine;
+                    }
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                inputText.setText(myData);
+                response.setText("SampleFile.txt data retrieved from External Storage...");
+            }
+        });
+
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            saveButton.setEnabled(false);
         }
+        else {
+            myExternalFile = new File(getExternalFilesDir(filepath), filename);
+        }
+
+
     }
+    private static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
